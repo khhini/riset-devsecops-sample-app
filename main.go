3f68a4cf7962d4f73276dc4bbd349e873ca528e4
khhini <kiki.h.hutapea@gmail.com>
2023-09-04 19:58:20 +0700

    Revert "feat: add docker definitions & app configs"
    This reverts commit b4f4520ed89ef5dc5080907d83bd661c5cc3f673.

--- main.go
+++ main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/khhini/riset-devsecops-sample-app/configs"
-	apigateway "github.com/khhini/riset-devsecops-sample-app/modules/api-gateway"
 	"github.com/khhini/riset-devsecops-sample-app/modules/hello"
 )
 
@@ -19,9 +17,6 @@ func SetupRouter() *gin.Engine {
 }
 
 func main() {
-	cfg := configs.DefaultListenConfig()
-	cfg.LoadFromEnv()
-
 	router := SetupRouter()
-	router.Run(cfg.Addr())
+	router.Run()
 }
